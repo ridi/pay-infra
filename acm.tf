e986521e27243f156a53cc22c29aaa2365d5234c
Hyeonsu Lee <incleaf@gmail.com>
2018-10-24 02:26:50 +0900

    Update domain name of ACM certificates

--- acm.tf
+++ acm.tf
@@ -1,8 +1,8 @@
 data "aws_acm_certificate" "cert" {
-  domain = "*.pay.ridibooks.com"
+  domain = "pay.ridibooks.com"
 }
 
 data "aws_acm_certificate" "cert-us-east-1" {
-  domain = "*.pay.ridibooks.com"
+  domain = "pay.ridibooks.com"
   provider = "aws.us-east-1"
 }
