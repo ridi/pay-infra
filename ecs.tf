0de5d4ceddfaab65338a62d654a081772da566f2
Gyu Kang <me@amolorsy.me>
2018-10-24 14:13:07 +0900

    Fix ecs container instance registration errors

--- ecs.tf
+++ ecs.tf
@@ -37,12 +37,19 @@ resource "aws_ecs_task_definition" "api" {
   }
 ]
 DEFINITION
-  task_role_arn = "${aws_iam_role.ecs_tasks_trust_role.arn}"
 }
 
+
 resource "aws_launch_configuration" "ecs_launch_configuration" {
+  name_prefix = "ridi-pay-ecs-"
   image_id = "${data.aws_ami.amazon_ecs_optimized.id}"
   instance_type = "t2.micro"
+  iam_instance_profile = "ecsInstanceRole"
+  security_groups = ["${aws_vpc.vpc.default_security_group_id}"]
+  user_data = <<EOF
+#!/bin/bash
+echo ECS_CLUSTER=${aws_ecs_cluster.ridi_pay.name} >> /etc/ecs/ecs.config
+EOF
   lifecycle {
     create_before_destroy = true
   }
@@ -52,9 +59,18 @@ resource "aws_autoscaling_group" "ecs_autoscaling_group" {
   max_size = 1
   min_size = 1
   desired_capacity = 1
+  availability_zones = ["ap-northeast-2a", "ap-northeast-2c"]
   launch_configuration = "${aws_launch_configuration.ecs_launch_configuration.name}"
   vpc_zone_identifier = [
     "${aws_subnet.private_2a.id}",
     "${aws_subnet.private_2c.id}"
   ]
+  tag {
+    key = "Name"
+    value = "${aws_launch_configuration.ecs_launch_configuration.name}"
+    propagate_at_launch = true
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
 }
