150071254fd86bb062f071fe3123b5d976b96f15
Gyu Kang <me@amolorsy.me>
2018-11-04 15:53:10 +0900

    Use dynamic port mapping for fluentd

--- alb.tf
+++ alb.tf
@@ -17,6 +17,22 @@ resource "aws_alb" "ridi_pay_backend" {
   }
 }
 
+resource "aws_lb" "ridi_pay_backend_fluentd" {
+  internal = true
+  load_balancer_type = "network"
+  name = "ridi-pay-backend-fluentd-${module.global_variables.env}"
+  subnets = [
+    "${aws_subnet.private_2a.id}",
+    "${aws_subnet.private_2c.id}"
+  ]
+  lifecycle {
+    create_before_destroy = true
+  }
+  tags {
+    Environment = "${module.global_variables.env}"
+  }
+}
+
 resource "aws_alb_target_group" "ridi_pay_backend" {
   port = 80
   protocol = "HTTP"
@@ -30,6 +46,15 @@ resource "aws_alb_target_group" "ridi_pay_backend" {
   }
 }
 
+resource "aws_lb_target_group" "ridi_pay_backend_fluentd" {
+  port = 24224
+  protocol = "TCP"
+  vpc_id = "${aws_vpc.vpc.id}"
+  depends_on = [
+    "aws_lb.ridi_pay_backend_fluentd"
+  ]
+}
+
 resource "aws_alb_listener" "ridi_pay_backend" {
   load_balancer_arn = "${aws_alb.ridi_pay_backend.arn}"
   port = 443
@@ -39,5 +64,15 @@ resource "aws_alb_listener" "ridi_pay_backend" {
   default_action {
     type = "forward"
     target_group_arn = "${aws_alb_target_group.ridi_pay_backend.arn}"
+  }
+}
+
+resource "aws_lb_listener" "ridi_pay_backend_fluentd" {
+  load_balancer_arn = "${aws_lb.ridi_pay_backend_fluentd.arn}"
+  port = 24224
+  protocol = "TCP"
+  default_action {
+    type = "forward"
+    target_group_arn = "${aws_lb_target_group.ridi_pay_backend_fluentd.arn}"
   }
 }
--- ecs.tf
+++ ecs.tf
@@ -3,9 +3,9 @@ resource "aws_ecr_repository" "ridi_pay_backend" {
   name = "ridi/pay-backend"
 }
 
-resource "aws_ecr_repository" "ridi_pay_logger" {
+resource "aws_ecr_repository" "ridi_pay_backend_fluentd" {
   count = "${module.global_variables.is_prod ? 1 : 0}"
-  name = "ridi/pay-logger"
+  name = "ridi/pay-backend-fluentd"
 }
 
 resource "aws_ecs_cluster" "ridi_pay_backend" {
@@ -32,6 +32,26 @@ EOF
   }
 }
 
+resource "aws_launch_configuration" "ridi_pay_backend_fluentd" {
+  name_prefix = "ridi-pay-backend-fluentd-ecs-"
+  image_id = "${data.aws_ami.amazon_ecs_optimized.id}"
+  instance_type = "t2.micro"
+  iam_instance_profile = "ecsInstanceRole"
+  key_name = "bastion"
+  security_groups = [
+    "${aws_vpc.vpc.default_security_group_id}",
+    "${aws_security_group.ssh_from_bastion.id}"
+  ]
+  user_data = <<EOF
+#!/bin/bash
+echo ECS_CLUSTER=${aws_ecs_cluster.ridi_pay_backend.name} >> /etc/ecs/ecs.config
+echo ECS_INSTANCE_ATTRIBUTES={\"service\":\"fluentd\"} >> /etc/ecs/ecs.config
+EOF
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
 resource "aws_autoscaling_group" "ridi_pay_backend" {
   max_size = 1
   min_size = 1
@@ -48,6 +68,29 @@ resource "aws_autoscaling_group" "ridi_pay_backend" {
   tag {
     key = "Name"
     value = "${aws_launch_configuration.ridi_pay_backend.name}"
+    propagate_at_launch = true
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "ridi_pay_backend_fluentd" {
+  max_size = 1
+  min_size = 1
+  desired_capacity = 1
+  availability_zones = [
+    "ap-northeast-2a",
+    "ap-northeast-2c"
+  ]
+  launch_configuration = "${aws_launch_configuration.ridi_pay_backend_fluentd.name}"
+  vpc_zone_identifier = [
+    "${aws_subnet.private_2a.id}",
+    "${aws_subnet.private_2c.id}"
+  ]
+  tag {
+    key = "Name"
+    value = "${aws_launch_configuration.ridi_pay_backend_fluentd.name}"
     propagate_at_launch = true
   }
   lifecycle {
