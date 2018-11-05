2a4c47759c2e914824ad590714d766772190f95b
Gyu Kang <me@amolorsy.me>
2018-11-05 12:41:29 +0900

    Move a backend api document to the ridi-pay aws account

--- s3.tf
+++ s3.tf
@@ -6,3 +6,37 @@ resource "aws_s3_bucket_policy" "ridi_pay_frontend" {
   bucket = "${aws_s3_bucket.ridi_pay_frontend.id}"
   policy = "${data.aws_iam_policy_document.ridi_pay_frontend.json}"
 }
+
+resource "aws_s3_bucket" "ridi_pay_backend_api_doc" {
+  count = "${module.global_variables.is_prod ? 1 : 0}"
+  bucket = "ridi-pay-backend-api-doc"
+}
+
+resource "aws_s3_bucket_policy" "ridi_pay_backend_api_doc" {
+  bucket = "${aws_s3_bucket.ridi_pay_backend_api_doc.id}"
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": "s3:GetObject",
+            "Resource": "arn:aws:s3:::ridi-pay-backend-api-doc/*",
+            "Condition": {
+                "IpAddress": {
+                    "aws:SourceIp": [
+                      "218.232.41.2/32",
+                      "218.232.41.3/32",
+                      "218.232.41.4/32",
+                      "218.232.41.5/32",
+                      "222.231.4.164/32",
+                      "222.231.4.165/32"
+                    ]
+                }
+            }
+        }
+    ]
+}
+POLICY
+}
