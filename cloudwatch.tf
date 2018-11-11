e0c98dc93c2d96ead0bbdadfb2ae46f8763f153f
Gyu Kang <me@amolorsy.me>
2018-11-12 00:36:49 +0900

    Add cloudwatch alarm actions

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -22,6 +22,10 @@ resource "aws_cloudwatch_log_group" "backend_api_pg_kcp" {
   }
 }
 
+data "aws_sns_topic" "cloudwatch_alarm" {
+  name = "cloudwatch-alarm"
+}
+
 resource "aws_cloudwatch_metric_alarm" "backend_api_alb_5xx_error" {
   count = "${module.global_variables.is_prod ? 1 : 0}"
   alarm_name = "backend_api_alb_5xx_error"
@@ -32,6 +36,7 @@ resource "aws_cloudwatch_metric_alarm" "backend_api_alb_5xx_error" {
   period = "300"
   statistic = "Sum"
   threshold = "10"
+  alarm_actions = ["${data.aws_sns_topic.cloudwatch_alarm.arn}"]
   alarm_description = "최근 5분 동안 API ALB 5xx Error(Target 5xx 에러 제외) 10건 이상 발생"
   datapoints_to_alarm = 1
   dimensions {
@@ -49,6 +54,7 @@ resource "aws_cloudwatch_metric_alarm" "backend_api_cpu_utilization" {
   period = "300"
   statistic = "Average"
   threshold = "70"
+  alarm_actions = ["${data.aws_sns_topic.cloudwatch_alarm.arn}"]
   alarm_description = "최근 5분 동안 api service 평균 CpuUtilization 70% 초과"
   datapoints_to_alarm = 1
   dimensions {
@@ -67,6 +73,7 @@ resource "aws_cloudwatch_metric_alarm" "backend_api_memory_utilization" {
   period = "300"
   statistic = "Average"
   threshold = "70"
+  alarm_actions = ["${data.aws_sns_topic.cloudwatch_alarm.arn}"]
   alarm_description = "최근 5분 동안 api service 평균 MemoryUtilization 70% 초과"
   datapoints_to_alarm = 1
   dimensions {
@@ -85,6 +92,7 @@ resource "aws_cloudwatch_metric_alarm" "backend_fluentd_cpu_utilization" {
   period = "300"
   statistic = "Average"
   threshold = "70"
+  alarm_actions = ["${data.aws_sns_topic.cloudwatch_alarm.arn}"]
   alarm_description = "최근 5분 동안 fluentd service 평균 CpuUtilization 70% 초과"
   datapoints_to_alarm = 1
   dimensions {
@@ -103,6 +111,7 @@ resource "aws_cloudwatch_metric_alarm" "backend_fluentd_memory_utilization" {
   period = "300"
   statistic = "Average"
   threshold = "70"
+  alarm_actions = ["${data.aws_sns_topic.cloudwatch_alarm.arn}"]
   alarm_description = "최근 5분 동안 fluentd service 평균 MemoryUtilization 70% 초과"
   datapoints_to_alarm = 1
   dimensions {
@@ -121,6 +130,7 @@ resource "aws_cloudwatch_metric_alarm" "elasticache_001_cpu_utilization" {
   period = "300"
   statistic = "Average"
   threshold = "45"
+  alarm_actions = ["${data.aws_sns_topic.cloudwatch_alarm.arn}"]
   alarm_description = "최근 5분 동안 ElastiCache-001 평균 CPUUtilization vCPU당 45%, 총 90% 이상 / https://docs.aws.amazon.com/ko_kr/AmazonElastiCache/latest/red-ug/CacheMetrics.WhichShouldIMonitor.html 참고"
   datapoints_to_alarm = 1
   dimensions {
@@ -139,6 +149,7 @@ resource "aws_cloudwatch_metric_alarm" "elasticache_001_swap_usage" {
   period = "300"
   statistic = "Maximum"
   threshold = "${50 * 1024 * 1024}"
+  alarm_actions = ["${data.aws_sns_topic.cloudwatch_alarm.arn}"]
   alarm_description = "최근 5분 동안 ElastiCache-001 최대 SwapUsage 50MB 이상 / https://docs.aws.amazon.com/ko_kr/AmazonElastiCache/latest/red-ug/CacheMetrics.WhichShouldIMonitor.html 참고"
   datapoints_to_alarm = 1
   dimensions {
@@ -157,6 +168,7 @@ resource "aws_cloudwatch_metric_alarm" "elasticache_001_freeable_memory" {
   period = "300"
   statistic = "Minimum"
   threshold = "${2 * 1024 * 1024 * 1024}"
+  alarm_actions = ["${data.aws_sns_topic.cloudwatch_alarm.arn}"]
   alarm_description = "최근 5분 동안 ElastiCache-001 최소 FreeableMemory 2GB(30%) 이하"
   datapoints_to_alarm = 1
   dimensions {
@@ -175,6 +187,7 @@ resource "aws_cloudwatch_metric_alarm" "elasticache_001_is_master" {
   period = "300"
   statistic = "Minimum"
   threshold = "1"
+  alarm_actions = ["${data.aws_sns_topic.cloudwatch_alarm.arn}"]
   alarm_description = "최근 5분 동안 ElastiCache-001 Node Fail 발생"
   datapoints_to_alarm = 1
   dimensions {
@@ -193,6 +206,7 @@ resource "aws_cloudwatch_metric_alarm" "rds_master_error_log" {
   period = "300"
   statistic = "Sum"
   threshold = "1"
+  alarm_actions = ["${data.aws_sns_topic.cloudwatch_alarm.arn}"]
   alarm_description = "최근 5분 동안 RDS error log 1건 이상 발생"
   datapoints_to_alarm = 1
   dimensions {
@@ -210,6 +224,7 @@ resource "aws_cloudwatch_metric_alarm" "rds_master_cpu_utilization" {
   period = "300"
   statistic = "Average"
   threshold = "70"
+  alarm_actions = ["${data.aws_sns_topic.cloudwatch_alarm.arn}"]
   alarm_description = "최근 5분 동안 RDS master 평균 CPUUtilization 70% 이상"
   datapoints_to_alarm = 1
   dimensions {
@@ -227,6 +242,7 @@ resource "aws_cloudwatch_metric_alarm" "rds_master_freeable_memory" {
   period = "300"
   statistic = "Minimum"
   threshold = "${100 * 1024 * 1024}"
+  alarm_actions = ["${data.aws_sns_topic.cloudwatch_alarm.arn}"]
   alarm_description = "최근 5분 동안 RDS master 최소 FreeableMemory 100MB 이하"
   datapoints_to_alarm = 1
   dimensions {
@@ -244,6 +260,7 @@ resource "aws_cloudwatch_metric_alarm" "rds_master_free_storage_space" {
   period = "300"
   statistic = "Minimum"
   threshold = "${75 * 1024 * 1024 * 1024}"
+  alarm_actions = ["${data.aws_sns_topic.cloudwatch_alarm.arn}"]
   alarm_description = "최근 5분 동안 RDS master 최소 FreeStorageSpace 75GB(30%) 이하"
   datapoints_to_alarm = 1
   dimensions {
@@ -261,6 +278,7 @@ resource "aws_cloudwatch_metric_alarm" "rds_master_swap_usage" {
   period = "300"
   statistic = "Maximum"
   threshold = "${100 * 1024 * 1024}"
+  alarm_actions = ["${data.aws_sns_topic.cloudwatch_alarm.arn}"]
   alarm_description = "최근 5분 동안 최대 SwapUsage 100MB 이상"
   datapoints_to_alarm = 1
   dimensions {
@@ -278,6 +296,7 @@ resource "aws_cloudwatch_metric_alarm" "rds_master_database_connections" {
   period = "300"
   statistic = "Maximum"
   threshold = "440"
+  alarm_actions = ["${data.aws_sns_topic.cloudwatch_alarm.arn}"]
   alarm_description = "최근 5분 동안 최대 DatabaseConnections 440개(70%) 이상"
   datapoints_to_alarm = 1
   dimensions {
