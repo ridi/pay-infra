c121cffed20a63fef7fce405074562273f181b7c
Gyu Kang <me@amolorsy.me>
2019-02-26 03:43:25 +0900

    Change rds and elasticache instance types

--- elasticache.tf
+++ elasticache.tf
@@ -1,16 +1,12 @@
 resource "aws_elasticache_replication_group" "redis" {
   count = "${module.global_variables.is_staging ? 0 : 1}"
   automatic_failover_enabled = "${module.global_variables.is_prod ? true : false}"
-  availability_zones = [
-    "ap-northeast-2a",
-    "ap-northeast-2c"
-  ]
   replication_group_id = "ridi-pay-${module.global_variables.env}"
   replication_group_description = "ridi-pay-${module.global_variables.env}"
   security_group_ids = [
     "${aws_vpc.vpc.default_security_group_id}"
   ]
-  node_type = "${module.global_variables.is_prod ? "cache.m5.large" : "cache.t2.micro"}"
+  node_type = "${module.global_variables.is_prod ? "cache.t2.medium" : "cache.t2.micro"}"
   number_cache_clusters = 2
   parameter_group_name = "default.redis4.0"
   port = 6379
