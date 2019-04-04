a5408cc7daebda057604ea4cf24f231fbdb300ae
Jihyeok Seo <limeburst@ridi.com>
2019-04-04 18:57:20 +0900

    Migrate state to Terraform Enterprise

--- terraform.tf
+++ terraform.tf
@@ -1,9 +1,11 @@
 terraform {
-  backend "s3" {
-    bucket         = "ridi-pay-terraform-state"
-    dynamodb_table = "terraform-state-lock"
-    region         = "ap-northeast-2"
-    key            = "terraform.tfstate"
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "ridi"
+
+    workspaces {
+      prefix = "pay-"
+    }
   }
 }
 
