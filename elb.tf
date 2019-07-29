59073d4615169369c0104119e81869ad8bccf540
Gyu Kang <me@amolorsy.me>
2019-07-29 19:53:14 +0900

    Add a alb and an alb listener for fargate

--- elb.tf
+++ elb.tf
@@ -18,6 +18,26 @@ resource "aws_alb" "ridi_pay_backend" {
   }
 }
 
+resource "aws_alb" "ridi_pay_backend_fargate" {
+  internal = false
+  load_balancer_type = "application"
+  name = "pay-backend-${module.global_variables.env}"
+  security_groups = [
+    aws_security_group.web.id,
+  ]
+  subnets = [
+    aws_subnet.public_2a.id,
+    aws_subnet.public_2c.id,
+  ]
+  enable_deletion_protection = true
+  lifecycle {
+    create_before_destroy = true
+  }
+  tags = {
+    Environment = module.global_variables.env
+  }
+}
+
 resource "aws_alb_target_group" "ridi_pay_backend" {
   port                 = 80
   protocol             = "HTTP"
@@ -36,9 +56,9 @@ resource "aws_alb_target_group" "ridi_pay_backend_fargate" {
   target_type = "ip"
   vpc_id = aws_vpc.vpc.id
   deregistration_delay = 60
-  depends_on = [aws_alb.ridi_pay_backend]
+  depends_on = [aws_alb.ridi_pay_backend_fargate]
   health_check {
-    path    = "/health-check"
+    path = "/health-check"
     matcher = "200"
   }
 }
@@ -52,5 +72,17 @@ resource "aws_alb_listener" "ridi_pay_backend" {
   default_action {
     type             = "forward"
     target_group_arn = aws_alb_target_group.ridi_pay_backend.arn
+  }
+}
+
+resource "aws_alb_listener" "ridi_pay_backend_fargate" {
+  load_balancer_arn = aws_alb.ridi_pay_backend_fargate.arn
+  port = 443
+  protocol = "HTTPS"
+  ssl_policy = "ELBSecurityPolicy-TLS-1-1-2017-01"
+  certificate_arn = data.aws_acm_certificate.cert.arn
+  default_action {
+    type = "forward"
+    target_group_arn = aws_alb_target_group.ridi_pay_backend_fargate.arn
   }
 }
