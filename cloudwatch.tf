34e86fb3a5ed64f7ec293de5773b3cff92a6f53d
Gyu Kang <me@amolorsy.me>
2019-08-05 20:23:43 +0900

    Delete unused cloudwatch log groups

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,27 +1,3 @@
-resource "aws_cloudwatch_log_group" "backend_api_apache_access" {
-  name = "${module.global_variables.env}.ridi-pay.backend.api.apache.access"
-
-  tags = {
-    Environment = module.global_variables.env
-  }
-}
-
-resource "aws_cloudwatch_log_group" "backend_api_apache_error" {
-  name = "${module.global_variables.env}.ridi-pay.backend.api.apache.error"
-
-  tags = {
-    Environment = module.global_variables.env
-  }
-}
-
-resource "aws_cloudwatch_log_group" "backend_api_pg_kcp" {
-  name = "${module.global_variables.env}.ridi-pay.backend.api.pg.kcp"
-
-  tags = {
-    Environment = module.global_variables.env
-  }
-}
-
 resource "aws_cloudwatch_log_group" "backend" {
   name = "${module.global_variables.env}.ridi-pay-backend"
   
