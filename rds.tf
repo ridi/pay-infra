aa87cae27c97c9b04a32e9e56bbbca867a0a3761
minq <minq@ridi.com>
2020-05-13 23:19:30 +0900

    Add logging

--- rds.tf
+++ rds.tf
@@ -46,6 +46,18 @@ resource "aws_db_parameter_group" "master" {
   name   = "ridi-pay-${module.global_variables.env}-master"
   family = "mariadb10.3"
   parameter {
+    name  = "general_log"
+    value = "1"
+  }
+  parameter {
+    name  = "slow_query_log"
+    value = "1"
+  }
+  parameter {
+    name  = "long_query_time"
+    value = "1"
+  }
+  parameter {
     name  = "read_only"
     value = "0"
   }
@@ -71,6 +83,18 @@ resource "aws_db_parameter_group" "slave" {
   count  = module.global_variables.is_prod ? 1 : 0
   name   = "ridi-pay-${module.global_variables.env}-slave"
   family = "mariadb10.3"
+  parameter {
+    name  = "general_log"
+    value = "1"
+  }
+  parameter {
+    name  = "slow_query_log"
+    value = "1"
+  }
+  parameter {
+    name  = "long_query_time"
+    value = "1"
+  }
   parameter {
     name  = "read_only"
     value = "1"
