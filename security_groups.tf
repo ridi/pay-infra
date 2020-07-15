4f01065df0156c4be609b8ed50d49976af96979c
minq <minq@ridi.com>
2020-07-15 17:31:07 +0900

    ISMS 결함 조치 사항으로 Bastion 아웃 바운드 포트 제한 (#12)

--- security_groups.tf
+++ security_groups.tf
@@ -1,20 +1,20 @@
 resource "aws_security_group" "fargate" {
   vpc_id = aws_vpc.vpc.id
-  name = "fargate"
+  name   = "fargate"
 
   ingress {
     from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    to_port   = 80
+    protocol  = "tcp"
     cidr_blocks = [
       aws_vpc.vpc.cidr_block
     ]
   }
-  
+
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -25,21 +25,21 @@ resource "aws_security_group" "fargate" {
 
 resource "aws_security_group" "ecr_vpc_endpoint" {
   vpc_id = aws_vpc.vpc.id
-  name = "ecr-vpc-endpoint"
+  name   = "ecr-vpc-endpoint"
 
   ingress {
     from_port = 443
-    to_port = 443
-    protocol = "tcp"
+    to_port   = 443
+    protocol  = "tcp"
     cidr_blocks = [
       aws_vpc.vpc.cidr_block
     ]
   }
-  
+
   egress {
     from_port = 0
-    to_port = 0
-    protocol = "-1"
+    to_port   = 0
+    protocol  = "-1"
     cidr_blocks = [
       aws_vpc.vpc.cidr_block
     ]
@@ -78,27 +78,39 @@ resource "aws_security_group" "bastion" {
   name   = "bastion"
   vpc_id = aws_vpc.vpc.id
   ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-    cidr_blocks = [
-      "218.232.41.2/32",
-      "218.232.41.3/32",
-      "218.232.41.4/32",
-      "218.232.41.5/32",
-      "222.231.4.164/32",
-      "222.231.4.165/32",
-    ]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.office_cidr_blocks
   }
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.office_cidr_blocks
   }
   tags = {
     Name = "bastion-${module.global_variables.env}"
   }
+}
+
+resource "aws_security_group_rule" "bastion_awslogs_outbound" {
+  type              = "egress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.bastion.id
+}
+
+resource "aws_security_group_rule" "bastion_internal_outbound_ports" {
+  type              = "egress"
+  count             = length(var.bastion_internal_outbound_ports)
+  from_port         = element(var.bastion_internal_outbound_ports, count.index)
+  to_port           = element(var.bastion_internal_outbound_ports, count.index)
+  protocol          = "tcp"
+  cidr_blocks       = [aws_vpc.vpc.cidr_block]
+  security_group_id = aws_security_group.bastion.id
 }
 
 resource "aws_security_group" "web" {
