b62ba9322345e66734f39f7972699cf0ceb78838
Jaeyong Kwack <jaeyongkwack@gmail.com>
2020-07-15 18:00:08 +0900

    added jaeyongkwack as a readonly-user (#11)

--- iam.tf
+++ iam.tf
@@ -22,16 +22,112 @@ data "aws_iam_policy_document" "ridi_pay_frontend" {
   }
 }
 
+# users
 resource "aws_iam_user" "hoseongson" {
   name = "hoseong.son"
 }
 
-resource "aws_iam_user_policy_attachment" "hoseongson_readonly" {
-  user       = aws_iam_user.hoseongson.name
+resource "aws_iam_user" "jaeyongkwack" {
+  name = "jaeyongkwack"
+}
+
+# developers group
+resource "aws_iam_group" "developers" {
+  name = "developers"
+  path = "/users/"
+}
+
+resource "aws_iam_group_membership" "developers" {
+  name = "developers"
+
+  users = [
+    aws_iam_user.hoseongson.name,
+    aws_iam_user.jaeyongkwack.name
+  ]
+
+  group = aws_iam_group.developers.name
+}
+
+resource "aws_iam_group_policy_attachment" "developers_read_only_policy_attachment" {
+  group      = aws_iam_group.developers.name
   policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
 }
 
-resource "aws_iam_user_policy_attachment" "hoseongson_iam_change_password" {
-  user       = aws_iam_user.hoseongson.name
-  policy_arn = "arn:aws:iam::aws:policy/IAMUserChangePassword"
+resource "aws_iam_group_policy" "developers_extra_policy" {
+  name  = "developers-extra-policy"
+  group = aws_iam_group.developers.id
+
+  policy = <<EOF
+{
+  "Version": "2020-07-01",
+  "Statement": [
+    {
+      "Action": [
+        "ssm:*"
+      ],
+      "Effect": "Deny",
+      "Resource": "*"
+    },
+    {
+      "Sid": "AllowViewAccountInfo",
+      "Effect": "Allow",
+      "Action": [
+          "iam:GetAccountPasswordPolicy",
+          "iam:GetAccountSummary",
+          "iam:ListVirtualMFADevices"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Sid": "AllowManageOwnPasswords",
+      "Effect": "Allow",
+      "Action": [
+          "iam:ChangePassword",
+          "iam:GetUser"
+      ],
+      "Resource": "arn:aws:iam::*:user/$${aws:username}"
+    },
+    {
+      "Sid": "ManageOwnAccessKeys",
+      "Effect": "Allow",
+      "Action": [
+        "iam:CreateAccessKey",
+        "iam:DeleteAccessKey",
+        "iam:GetAccessKeyLastUsed",
+        "iam:GetUser",
+        "iam:ListAccessKeys",
+        "iam:UpdateAccessKey"
+      ],
+      "Resource": "arn:aws:iam::*:user/$${aws:username}"
+    },
+    {
+      "Sid": "AllowManageOwnVirtualMFADevice",
+      "Effect": "Allow",
+      "Action": [
+          "iam:CreateVirtualMFADevice",
+          "iam:DeleteVirtualMFADevice"
+      ],
+      "Resource": "arn:aws:iam::*:mfa/$${aws:username}"
+    },
+    {
+      "Sid": "AllowManageOwnUserMFA",
+      "Effect": "Allow",
+      "Action": [
+          "iam:DeactivateMFADevice",
+          "iam:EnableMFADevice",
+          "iam:ListMFADevices",
+          "iam:ResyncMFADevice"
+      ],
+      "Resource": "arn:aws:iam::*:user/$${aws:username}"
+    },
+    {
+      "Action": [
+        "logs:PutQueryDefinition"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+    ]
+}
+EOF
 }
